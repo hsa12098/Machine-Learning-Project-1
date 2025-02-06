0badbc1915a7ffb26162ecbf3bc7c24519f3d49f
“hsa” <hamzasyed12098@gmail.com>
2025-02-06 07:05:07 +0500

    little updates regarding package setups

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 from setuptools import find_packages,setup
+from typing import List
+hyphen_e_dot = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this functon will return the list of requirements from the requirements.txt
+    '''
 
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements =  [req.replace("\n","")for req in requirements]
 
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+    return requirements
 
 setup(
 
@@ -9,6 +22,6 @@ version = '0.0.1',
 author = 'Hamza',
 author_email = 'hamzasyed12098@gmail.com',
 packages = find_packages(),
-install_requires = ['pandas','numpy','seaborn']
+install_requires = get_requirements('requirements.txt')
 
 )
